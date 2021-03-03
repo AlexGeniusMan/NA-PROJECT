53a39408056407790c42663c8aac3e1fa06603cc
AlexGeniusMan <alexgeniusman@gmail.com>
2021-03-03 16:48:56 +0300

    ReactAppView added

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 # from .permissions import *
 from .serializers import *
+import os
+from django.views.generic.base import View
+from django.http import HttpResponse
 
 
 class ShowRecentNewsView(APIView):
@@ -15,3 +18,21 @@ class ShowRecentNewsView(APIView):
         messages = MessageSerializer(messages, context={'request': request}, many=True).data
         return Response(messages)
         # return Response({"error_code": 'YOUR TICKET IS EXPIRED', "status": status.HTTP_403_FORBIDDEN})
+
+
+class ReactAppView(View):
+
+    def get(self, request):
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        try:
+            with open(os.path.join(BASE_DIR, 'frontend', 'build', 'index.html')) as file:
+                return HttpResponse(file.read())
+
+        except:
+            return HttpResponse(
+                """
+                File index.html not found ! Build your React app !
+                """,
+                status=501,
+            )
